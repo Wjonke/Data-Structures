8364ee32c400b22eb740170a5990d18d0dc5e242
wjonke <wesjonke@gmail.com>
2020-03-23 17:44:22 -0400

    Added functionality to queues.py, ran and passed tests.

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -6,14 +6,22 @@ from doubly_linked_list import DoublyLinkedList
 class Queue:
     def __init__(self):
         self.size = 0
+        self.storage = DoublyLinkedList() #setting storage used to an empty DLL instance
+        #storage now references DLL file
+        # we can call the methods we created there inside here
+
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+            # Because a DLL can very efficiently add and remove things without shifting.
+            #its worse at access but we dont really need to access anything specifically or sort it
+            # Tail is the bqack of the queue and head is the front
 
-    def enqueue(self, value):
-        pass
+    def enqueue(self, value): #insert, add to back of queue
+        self.storage.add_to_tail(value)
 
-    def dequeue(self):
-        pass
+    def dequeue(self): #delete
+        #grab what is being deleted, set it to a variable and return it, that way we have access to it's value after delete
+        self.size -= 1
+        return self.storage.remove_from_head()
 
-    def len(self):
-        pass
+    def len(self): #track length of queue
+        return self.storage.length
